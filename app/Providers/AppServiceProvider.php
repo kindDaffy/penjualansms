a665fa121048a7a558692b23aeaf0cb216dcae5f
kindDaffy <dff3003@gmail.com>
2025-01-20 12:50:45 +0700

    Perbaikan Dasbor Admin + Menambah Page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +23,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Inertia::share('auth', function () {
+        return [
+            'user' => Auth::check() ? [
+                'id' => Auth::id(),
+                'name' => Auth::user()->name,
+                'email' => Auth::user()->email,
+                'role' => Auth::user()->role,
+            ] : null,
+        ];
+    });
         Vite::prefetch(concurrency: 3);
     }
+
+    protected function authenticated(Request $request, $user)
+{
+    if ($user->role === 'admin') {
+        return redirect()->route('admin');
+    }
+
+    return redirect()->route('AdminDashboard');
+}
 }
